a9c8d251416ed2449c2075f39feef5cb98097353
liuronghui_zzu <79138965@qq.com>
2021-11-08 20:24:40 +0800

    1.version

---
+++ idea/src/learn_01/Method.java
@@ -0,0 +1,15 @@
+//package learn_01;
+//
+//public class Method {
+//    public static void main(String[] args) {
+//        isEvenNumber();
+//    }
+//    public static void isEvenNumber() {
+//        int number = 10;
+//        if(number%2 == 0){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+//    }
+//}
---
+++ idea/src/learn_01/reverse.java
@@ -0,0 +1,18 @@
+/*
+package learn_01;
+
+public class reverse {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        for(int start=0,end=arr.length-1;start<=end; start++,end--) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        }
+        for(int x:arr) {
+            System.out.print(x);
+        }
+    }
+
+}
+*/
